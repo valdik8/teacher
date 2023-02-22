af7d6ff6cdfec87d51c56fd275d8003e35ae9999
valdik8 <126060036+valdik8@users.noreply.github.com>
2023-02-22 11:36:43 +0300

    first cal

---
+++ main.py
@@ -0,0 +1,17 @@
+def cal():
+    num1 = int(input())
+    num2 = int(input())
+
+    operat = input()
+
+    if(operat == "+"):
+        print(num1 + num2)
+    if(operat == "-"):
+        print(num1 - num2)
+    if(operat == "*"):
+        print(num1 * num2)
+    if(operat == "/"):
+        print(num1 / num2)
+
+
+cal()
